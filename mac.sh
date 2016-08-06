3d71b0fea43fc74f7e14a40c03588b374f17036f
stefan judis <stefanjudis@gmail.com>
2016-08-06 02:39:48 +0200

    clean up and add more mac settings

---
+++ mac.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+
+echo -e "\n\nSetting OS X settings"
+echo "=============================="
+
+# echo "Finder: show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# echo "show hidden files by default"
+defaults write com.apple.Finder AppleShowAllFiles -bool false
+
+# echo "only use UTF-8 in Terminal.app"
+defaults write com.apple.terminal StringEncodings -array 4
+
+# echo "expand save dialog by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# echo "show the ~/Library folder in Finder"
+chflags nohidden ~/Library
+
+echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+echo "Enable subpixel font rendering on non-Apple LCDs"
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+echo "Enable automatically hide and show the Dock"
+defaults write com.apple.dock autohide -bool true
+
+echo "Enable Show remaining battery time;"
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write com.apple.menuextra.battery ShowTime -string "NO"
+
+echo "Use current directory as default search scope in Finder"
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+echo "Show Path bar in Finder"
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "Show Status bar in Finder"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "Set a blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 2
+
+echo "Set a shorter Delay until key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
