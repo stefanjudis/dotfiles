14f81a2aa72f6448fe3358769453af99350dd893
stefan judis <stefanjudis@gmail.com>
2017-11-30 10:31:37 +0100

    add npm-reset alias

--- aliases.zsh
+++ aliases.zsh
@@ -39,6 +39,7 @@ alias nt='npm test'
 alias nit='npm install && npm test'
 alias nk='npm link'
 alias nr='npm run'
+alias npm-reset='rm -rf node_modules && rm -f package-lock.json && npm install && npx node-notifier-cli -t "Done" -m "npm modules reinstalled" -s Glass -i https://cdn.rawgit.com/npm/logos/31945b5c/npm%20square/n-64.png'
 alias dnm='rm -rf node_modules && npm i'
 
 
