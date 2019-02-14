16c3bde5e3ceedb88f4351327f2f2f7eeecfa01b
stefan judis <stefanjudis@gmail.com>
2019-02-14 09:36:50 +0100

    Cleanup and more cask tasks

--- brew.sh
+++ brew.sh
@@ -25,39 +25,9 @@ brew install jq
 
 # *********************************
 # ffmpeg for quick video compression
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook suspicious-package quicklookase qlvideo
 
 # *********************************
 # ffmpeg for quick video compression
 brew install ffmpeg --with-vpx --with-vorbis --with-libvorbis --with-vpx --with-vorbis --with-theora --with-libogg --with-libvorbis --with-gpl --with-version3 --with-nonfree --with-postproc --with-libaacplus --with-libass --with-libcelt --with-libfaac --with-libfdk-aac --with-libfreetype --with-libmp3lame --with-libopencore-amrnb --with-libopencore-amrwb --with-libopenjpeg --with-openssl --with-libopus --with-libschroedinger --with-libspeex --with-libtheora --with-libvo-aacenc --with-libvorbis --with-libvpx --with-libx264 --with-libxvid
 
-# *********************************
-brew cask install graphql-playground
-
-# *********************************
-# markdown client
-brew cask install typora
-
-# *********************************
-# a better `cat`
-brew install bat
-
-# *********************************
-# a better `cd`
-brew install autojump
-
-# *********************************
-brew install exa
-
-# *********************************
-brew tap jeroenknoops/tap
-brew install gitin
-
-# *********************************
-brew cask install firefox
-
-# *********************************
-brew cask install iterm2
-
-# *********************************
-brew cask install alfred
--- cask.sh
+++ cask.sh
@@ -32,3 +32,28 @@ brew cask install qlmarkdown
 echo "installing fira code"
 brew tap caskroom/fonts
 brew cask install font-fira-code
+
+# *********************************
+brew cask install graphql-playground
+
+# *********************************
+# markdown client
+brew cask install typora
+
+# *********************************
+# a better `cat`
+brew install bat
+
+# *********************************
+# a better `cd`
+brew install autojump
+brew install exa
+
+brew tap jeroenknoops/tap
+brew install gitin
+
+brew cask install firefox
+brew cask install iterm2
+brew cask install alfred
+brew cask install wavebox
+brew cask install zoomus
