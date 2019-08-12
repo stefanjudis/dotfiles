f371e6291cfbc6d25233c81e81e330555e6b5b86
stefan judis <stefanjudis@gmail.com>
2019-08-12 18:43:28 +0200

    Start managing vscode extensions via dotfiles

---
+++ code.sh
@@ -0,0 +1,32 @@
+# https://marketplace.visualstudio.com/items?itemName=wangweixuan.yithemes
+code --install-extension wangweixuan.yithemes
+
+# https://marketplace.visualstudio.com/items?itemName=fabianlauer.vs-code-xml-format
+code --install-extension fabianlauer.vs-code-xml-format
+
+# https://marketplace.visualstudio.com/items?itemName=teabyii.ayu
+# icons
+code --install-extension teabyii.ayu
+
+# https://marketplace.visualstudio.com/items?itemName=octref.vetur
+code --install-extension octref.vetur
+
+# https://marketplace.visualstudio.com/items?itemName=pflannery.vscode-versionlens
+code --install-extension pflannery.vscode-versionlens
+
+# https://marketplace.visualstudio.com/items?itemName=be5invis.toml
+code --install-extension be5invis.toml
+
+# https://marketplace.visualstudio.com/items?itemName=WallabyJs.quokka-vscode
+code --install-extension wallabyjs.quokka-vscode
+
+# https://marketplace.visualstudio.com/items?itemName=rebornix.project-snippets
+code --install-extension rebornix.project-snippets
+
+# https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+code --install-extension esbenp.prettier-vscode
+
+
+
+
+
