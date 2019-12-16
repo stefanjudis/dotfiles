f348243b21e18a3ce30edc06469152ae04b45206
stefan judis <stefanjudis@gmail.com>
2019-12-16 10:26:32 +0100

    Show hidden files by default

--- mac.sh
+++ mac.sh
@@ -10,7 +10,7 @@ echo ""
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # echo "show hidden files by default"
-defaults write com.apple.Finder AppleShowAllFiles -bool false
+defaults write com.apple.Finder AppleShowAllFiles -bool true
 
 # echo "only use UTF-8 in Terminal.app"
 defaults write com.apple.terminal StringEncodings -array 4
