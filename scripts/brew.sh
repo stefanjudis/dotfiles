04ecaa63fbcce6b7aaa350a53ec1b3b78ee48365
stefan judis <stefanjudis@gmail.com>
2020-07-13 21:45:39 +0200

    Add coreutils to brew packages

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -27,6 +27,7 @@ brew_install_or_upgrade git-delta
 brew_install_or_upgrade go
 brew_install_or_upgrade shellcheck
 brew_install_or_upgrade fzf
+brew_install_or_upgrade coreutils
 
 # ffmpeg for quick video compression
 brew install ffmpeg --with-vorbis --with-libvorbis --with-vorbis --with-theora --with-libogg --with-libvorbis --with-gpl --with-version3 --with-nonfree --with-postproc --with-libaacplus --with-libass --with-libcelt --with-libfaac --with-libfdk-aac --with-libfreetype --with-libmp3lame --with-libopencore-amrnb --with-libopencore-amrwb --with-libopenjpeg --with-openssl --with-libopus --with-libschroedinger --with-libspeex --with-libtheora --with-libvo-aacenc --with-libvorbis --with-libvpx --with-libx264 --with-libxvid
