a900d7a5854c8e7c83ed6b8834bdbbb1b3c68de5
stefan judis <stefanjudis@gmail.com>
2021-09-18 16:51:44 +0200

    Fix .bin symlink creation

--- commands/install.sh
+++ commands/install.sh
@@ -41,6 +41,7 @@ FROM_DIR="$CONFIG_DIR/.bin"
 TARGET_DIR=~/.bin
 log_section_start "Sym linking $FROM_DIR to $TARGET_DIR"
 
+rm "$TARGET_DIR"
 symlink "$FROM_DIR" "$TARGET_DIR"
 
 # *************************************
